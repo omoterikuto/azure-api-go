91d478a90e0c2226b7951410f2305beb41b330fa
Omote Rikuto <omote_rikuto@cyberagent.co.jp>
2025-03-13 16:34:15 +0900

    first

--- go.mod
+++ go.mod
@@ -3,10 +3,20 @@ module azureapi
 go 1.24.1
 
 require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/cheggaaa/pb/v3 v3.1.7 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/fatih/color v1.18.0 // indirect
 	github.com/go-resty/resty/v2 v2.16.5 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-resty/resty/v2"
 	"github.com/joho/godotenv"
+	"github.com/manifoldco/promptui"
 )
 
 type Message struct {
@@ -26,30 +29,79 @@ type ResponseBody struct {
 	} `json:"choices"`
 }
 
-func main() {
+var (
+	apiKey      string
+	apiEndpoint string
+)
+
+func loadEnv() error {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		return err
 	}
-	apiKey := os.Getenv("OPENAI_API_KEY")
+	apiKey = os.Getenv("OPENAI_API_KEY")
 	if apiKey == "" {
-		log.Fatalf("OPENAI_API_KEY not set in environment")
+		return err
 	}
-	apiEndpoint := os.Getenv("OPENAI_API_ENDPOINT")
+	apiEndpoint = os.Getenv("OPENAI_API_ENDPOINT")
 	if apiEndpoint == "" {
-		log.Fatalf("OPENAI_API_ENDPOINT not set in environment")
+		return err
 	}
-	fmt.Println("現在地は？")
-	var location string
-	if _, err := fmt.Scan(&location); err != nil {
-		log.Fatalf("Error reading location: %v", err)
-	}
-	findRestaurant(apiKey, apiEndpoint, location)
+	return nil
 }
 
-func findRestaurant(apiKey, apiEndpoint, location string) {
+var marks = []string{"|", "/", "-", "\\"}
+
+func mark(i int) string {
+	return marks[i%len(marks)]
+}
+
+func main() {
+	if err := loadEnv(); err != nil {
+		log.Fatalf("Error loading environment variables: %v", err)
+	}
+	promptLocation := promptui.Prompt{
+		Label: "現在地",
+	}
+	location, err := promptLocation.Run()
+	if err != nil {
+		log.Fatalf("Error reading location: %v", err)
+	}
+	promptFee := promptui.Select{
+		Label: "希望金額",
+		Items: []string{"~1000円", "1000円~3000円", "3000円~5000円", "5000円~10000円", "10000円~"},
+	}
+	_, fee, err := promptFee.Run()
+	if err != nil {
+		log.Fatalf("Error reading fee: %v", err)
+	}
+	promptFavorite := promptui.Select{
+		Label: "料理",
+		Items: []string{"和食", "イタリアン", "中華", "洋食", "ファストフード"},
+	}
+	_, favorite, err := promptFavorite.Run()
+	if err != nil {
+		log.Fatalf("Error reading fee: %v", err)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	ticket := time.NewTicker(100 * time.Millisecond)
+	var count int
+	go func() {
+		for ; ; count++ {
+			select {
+			case <-ctx.Done():
+				break
+			case <-ticket.C:
+				fmt.Printf("\rおすすめのレストランを検索中 %s", mark(count))
+			}
+		}
+	}()
+	findRestaurant(cancel, location, fee, favorite)
+}
+
+func findRestaurant(cancelFunc context.CancelFunc, location, fee, favorite string) {
 	client := resty.New()
-	prompt := fmt.Sprintf("%s 付近のおすすめのレストランを3つ教えて", location)
+	prompt := fmt.Sprintf("%s 付近のおすすめの%sレストランを3つ教えて。金額は%sで探して。", favorite, location, fee)
 	requestBody := RequestBody{
 		Messages: []Message{
 			{
@@ -67,7 +119,7 @@ func findRestaurant(apiKey, apiEndpoint, location string) {
 		SetHeader("api-key", apiKey).
 		SetBody(jsonData).
 		Post(fmt.Sprintf("%s/openai/deployments/gpt-4o-server/chat/completions?api-version=2024-02-01", apiEndpoint))
-
+	cancelFunc()
 	if err != nil {
 		log.Fatalf("Error making API request: %v", err)
 	}
